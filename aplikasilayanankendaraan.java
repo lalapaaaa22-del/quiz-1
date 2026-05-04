a8185d13e5aeea2fb29aa6ea90cedc199f488df5
jichuyaaaa <lalapaaaa22@gmail.com>
2026-05-04 14:30:32 +0700

    ubahkoreksi

--- aplikasilayanankendaraan.java
+++ aplikasilayanankendaraan.java
@@ -18,23 +18,22 @@ public class aplikasilayanankendaraan {
     //5.
 }
 //1. tentukan class apa saja yg dibutuhkan dan jelaskan alasan pemilihannya
-jawaban : ada class produk, ada sub class mobil dan motor . lalu ada class 
+jawaban : ada class kendaraan nomor polisi , merek tahunoridksi , jumlahpintustatus servis   ada sub class mobil dengan jumlah pintu dan tipe transakasi dan motor dengan jenismesin dan kapasitas tangki.
           class pemasukan untuk sebagai tanda terima untuk data catatan layanan kendaraan 
           class main ada untuk produknya 
     
 //2.identifikasi atribut Dan method yg ditampilkan di class induk dan turunan
-jawaban : ada total pemasukkan, ada penentuan jenis kendaraan, pengisian jawaban yang data umum yang perlu diisi
-            dengan methodnya adalah pemasukan, produk,mobil, motor dan kasir
-
+jawaban : 
+nomor polisi , merek tahunoridksi , jumlahpintustatus servis  dan method getNomorpolisi, getMerek,getTahunproduksi dan lain lainnya
 //3.bagaimana constructor digunakan agar objek kendaraan selelu memiliki data awal yg valid
-jawaban: objek harus ada di dalam class dan harus ada yang memanggil objek sebagai proses dari return atau mengembalikan 
-
+jawaban: constuctor harus diisi nomor polisi merek dan tahub produksi 
+pakek this nomorPolisi = nomorPolisi;
 //4. analisi bagaimana enkapulasi ditetapkan untuk melindungi data kendaraan khususnya pada  atribut data servis 
-    enkapulasi ditetapkan dengan cara penentuan dari objeknya dan di buat bentuk private bukan publik agar data servis tetap aman dan tidak dicampuri
-
+  dengan di private dengan getter baca data setter data mendapatkan data  contoh stqtaus servisnya di private
 //5. jelaskan mengapa inheritance dapat digunakan pada kasus ini serta alasannya  lebih cocok menjadi sub class dibandingkan membuat class mobil dan motor secara terpisah
-karna ada class induk dan turunan dimana class turunan adalah bukti fungsi dari inheritance yaitu menurunkan sehingga kasus ini lebih cocok menggunakan subclass dari pada membeuat class mobil dan motor secara langsung 
-
+karna mobil dan motor punya atribut yang sama jadi bisa menghemat kode dengan inheritance kkarna inheritance itu untuk mewariskan atau menurunkan
 //6. berikan minimal 1 contoh objek nyata dari class mobil dan motor lalu jelaskan nilai atribur yang dimiliki objek tersebut 
-jawaban : objek ada karna proses return contohnya dan merupakan nilai yang  ingin dicari contohnya
-jumlah pertemuan dalam data layanan kendaraan untuk mengetahui perkembangan dari servis atau menentukan status servis kendaraan 
+jawaban : misal : mobil, x234567 , Honda , 2024
+begitu juga dengan motor dengan atributnya nomor polisi , merek tahunoridksi , jumlahpintustatus servis  
+
+//ini koreksian:)
